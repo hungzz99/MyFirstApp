bd2716fe4543ebe0c405e7ab9bdc9258f5bf995f
Le Thanh Hung <hungltgch17366@fpt.edu.vn>
2019-04-26 17:19:20 +0700

    hungzz99
    .

--- index.php
+++ index.php
@@ -1,11 +1,33 @@
 <!DOCTYPE html>
 <html>
 <body>
-	<p><a href="https://www.facebook.com/binh.trinh.750">Tai Day</a></p>
 
-<?php
-echo "My first PHP script!";
+<h1>My first PHP page</h1>
+
+<?php 
+$sql = "SELECT id, name FROM label";
+
+$db = parse_url(getenv("DATABASE_URL"));
+
+$pdo = new PDO("pgsql:" . sprintf(
+    "host=%s;port=%s;user=%s;password=%s;dbname=%s",
+    $db["host"],
+    $db["port"],
+    $db["user"],
+    $db["pass"],
+    ltrim($db["path"], "/")
+));
+
+$stmt = $pdo->prepare($sql);
+//Thiết lập kiểu dữ liệu trả về
+$stmt->setFetchMode(PDO::FETCH_ASSOC);
+$stmt->execute();
+$resultSet = $stmt->fetchAll();
+
+foreach ($resultSet as $row) {
+	echo $row['name'] . '\n';
+}
+
 ?>
-<h2>Hello Tuan Anh</h2>
 </body>
 </html>
