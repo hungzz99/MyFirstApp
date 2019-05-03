0c3ad2fffa80d756bcddaecf1cc1875b3d97f9a2
Le Thanh Hung <hungltgch17366@fpt.edu.vn>
2019-05-03 14:38:40 +0700

    Update demo.php

--- demo.php
+++ demo.php
@@ -12,11 +12,24 @@
 		margin: 0 auto;
 	}
 	.button
-		{
-			float: right;
-			margin:11px;
-			padding-right: 40px;
-		}
+	{
+		float: right;
+		margin:11px;
+		padding-right: 40px;
+	}
+	.navbg
+	{
+		width: 100%;
+		height: 50px;
+		background-color: #C6BEBE;
+	}
+	.buttonnav
+	{
+		float: right;
+		font-size: 200%;
+		padding-right: 5px;
+	}
+
 
 	<form/>
 </style>
@@ -25,12 +38,22 @@
 	<div class="web">
 		<div class="button">
 			<div>
-				<form action="https://hungzz99.herokuapp.com/demo.php">
+				<form action="https://hungzz99.herokuapp.com/loign.php">
 						Hot line : 099-888-999
 						<input type="button" name="SignIn" value="Login">
 				</form>
 			</div>
 		</div>
 	</div>
+	<div class="navbg">
+			<form action="search.php" method="GET">
+			<div class="buttonnav"><input type="text" name="keyword" placeholder="Search...">
+				<input type="submit" name="Search" value="Search"></div>
+			</form>
+			<div class="nav">
+				<form action=""https://hungzz99.herokuapp.com/demo.php"">
+					<li>Products</li>
+				</form>
+			</div>
 </body>
 </html>
