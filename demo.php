ae2f4a8f1e17a7c1fef45560b0c3fa0718e41909
Le Thanh Hung <hungltgch17366@fpt.edu.vn>
2019-05-03 14:24:23 +0700

    Update demo.php

--- demo.php
+++ demo.php
@@ -11,7 +11,27 @@
 		width: 90%
 		margin: 0 auto;
 	}
+	.button
+		{
+			float: right;
+			margin:11px;
+			padding-right: 40px;
+		}
+
 	<form/>
 </style>
+</body>
+<body>
+	<div class="web">
+		<div class="button">
+			<div>
+				<form action="https://hungzz99.herokuapp.com/demo.php">
+						Hot line : 099-888-999
+						<input type="submit" name="SignUp" value="Sing Up">
+						<input type="button" name="SignIn" value="Login">
+				</form>
+			</div>
+		</div>
+	</div>
 </body>
 </html>
