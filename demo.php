b295ca4388868b84d6d9e6c8e38471bc3f413df5
Le Thanh Hung <hungltgch17366@fpt.edu.vn>
2019-05-04 11:10:04 +0700

    Update demo.php

--- demo.php
+++ demo.php
@@ -135,7 +135,7 @@
 	</div>
 		<div class="banner">
 			<p>Toy Store</p>
-			<div class="list">
+			<!-- <div class="list">
 				<?php
 			$db = pg_connect("host=ec2-174-129-208-118.compute-1.amazonaws.com port=5432 dbname=d6hnv52hdi9nnk user=gpjcnwwhasdiya password=e9144a959f5174b3271698c559c6c557bce4d71494e9599fa793d33ede9b9436");
 			$result = pg_query($db,"SELECT * FROM Products");
@@ -152,7 +152,7 @@
 				<?php
 				}
 				?> 
-		</div>
+		</div> -->
 
 
 </body>
