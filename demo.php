9454412922b6d66ffc5fa4a6ec76c744fd14f6d7
Le Thanh Hung <hungltgch17366@fpt.edu.vn>
2019-05-04 11:34:27 +0700

    Update demo.php

--- demo.php
+++ demo.php
@@ -3,112 +3,141 @@
 <head>
 	<title>Assingment2</title>
 	<style type="text/css">
-	.web
-	{
-		width: 90%
-		margin: 0 auto;
-	}
-	.button
-	{
-		float: right;
-		margin:11px;
-		padding-right: 40px;
-	}
-	.tittle
+	.Web
 		{
-			margin:10px;
+			width: 90%;
+			margin: 0 auto;
 		}
-	.navbg
-	{
-		width: 100%;
-		height: 50px;
-		background-color:  #6EE1E5;
-	}
-	.buttonnav
-	{
-		float: right;
-		font-size: 200%;
-		padding-right: 5px;
-	}
-	.nav
-	{ 
-		margin-left: 350px;
-		width: 70%;
-		height: 50px;
-		background-color: #6EE1E5;
-		align-self: auto;
-	}
-	.header
-	{
-		width: 100%;
-		height: 208px;
-		background-color:white;
-	}
-	.header img{
-		max-width: 90%;
-		max-height: 99%;
-	}
-	.banner
-	{
-		width: 100%;
-		height: 1200px;
-	}
-	.banner p
-	{
-		font-size: 80px;
-		color: blue;
-		text-align: center;
-		
-	}
+		.header
+		{
+			width: 100%;
+			height: 208px;
+			background-color:white;
+
+		}
+		.navbg{
+			width: 100%;
+			height: 50px;
+			background-color: #C6BEBE;
+
+		}
+		.nav
+		{ 	margin-left: 350px;
+			width: 70%;
+			height: 50px;
+			background-color: #C6BEBE;
+			align-self: auto;
+
+
+		}
+		.nav ul
+		{
+			margin: 0 auto;
+			padding: 1px 30px;
+			list-style: none;
+			align-content: auto;
+		}
+		.nav a
+		{
+			padding: 0 10px;
+			text-decoration: none;
+			font-size: 30px;
+			color: black;
+			display: block;
+			line-height: 50px;
+
+		}
+		.nav li
+		{	margin: auto;
+			float: left;
+			padding: 0px 20px;
+			
+		}
+		.nav a:hover
+		{
+			background-color: #52CA3A;
+			color: yellow;
+		}
+		.button
+		{
+			float: right;
+			margin:11px;
+			padding-right: 40px;
+		}
+		.buttonnav
+		{
+			float: right;
+			font-size: 200%;
+			padding-right: 5px;
+
+		}
+		.banner
+		{
+			width: 100%;
+			height: 1200px;
+
+		}
+		.banner p
+		{
+			font-size: 80px;
+			color: red;
+			text-align: center;
+			text-decoration: underline;
+
+		}
+		.list
+		{
+			width: 100%;
+			height: `1200px;
+			padding-top: 10px;
+			background-color: white;
+		}
+		.item
+		{
+			margin : 10px;
+			padding: 5px;
+			background-color: white;
+			width: 23%;
+			height: 320px;
+			float:left;
+			box-sizing: border-box;
+			text-align: center;
+		}
+		.item img
+		{
+			width: 200px;
+			height: 200px;
+		}
 		.image
 		{
 			margin: 10px;
 		}
-	.list
-	{
-		width: 100%;
-		height: `1200px;
-		padding-top: 10px;
-		background-color: white;
-	}
-	.item
-	{
-		margin : 10px;
-		padding: 5px;
-		background-color: white;
-		width: 23%;
-		height: 320px;
-		float:left;
-		box-sizing: border-box;
-		text-align: center;
-	}
-	.nav ul
-	{
-		margin: 0 auto;
-		padding: 1px 30px;
-		list-style: none;
-		align-content: auto;
-	}
-	.nav a
-	{
-		padding: 0 10px;
-		text-decoration: none;
-		font-size: 30px;
-		color: black;
-		display: block;
-		line-height: 50px;
-	}
-	.nav li
-	{	margin: auto;
-		float: left;
-		padding: 0px 20px;
+		.tittle
+		{
+			margin:10px;
+		}
+		.aaaa{
+			margin: 0 auto;
+			width: 90%;
+			text-align: center;
+			float:left;
+			text-align: left;
+
+		}
+		.iname
+		{
+			font-size: 50px;
+			font-weight: bold;
+			float: right;
 			
-	}
-	.nav a:hover
-	{
-		background-color: #52CA3A;
-		color: black;
-	}
+		}
+		.ides
+		{
+			font-size:30px;
+			float: right;
+			
+		}
+		
 </style>
 </head>
 <body>
