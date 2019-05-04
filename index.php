e334efaea5accb28b2c40bee12af380db2beea79
Le Thanh Hung <hungltgch17366@fpt.edu.vn>
2019-05-04 09:32:33 +0700

    v

--- index.php
+++ index.php
@@ -143,7 +143,7 @@
 			<div class="list">
 				<?php
 				$connection=pg_connect("host=ec2-174-129-208-118.compute-1.amazonaws.com port=5432 dbname=d6hnv52hdi9nnk user=gpjcnwwhasdiya password=e9144a959f5174b3271698c559c6c557bce4d71494e9599fa793d33ede9b9436")
-				$sql = "select * from Products";
+				$sql = "SELECT * FROM Products";
 				$result = pg_query($connection, $sql);
 				while ($rows=pg_fetch_assoc($result))
 				{
