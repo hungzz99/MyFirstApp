449a59533da5730fee48e8d16118532ff55ca4f7
Le Thanh Hung <hungltgch17366@fpt.edu.vn>
2019-05-04 09:46:21 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,10 +3,7 @@
 <html>
 <head>
 	<title>Demo</title>
-</head>
-<body>
-<style type="text/css">
-	<form>
+	<style type="text/css">
 	.web
 	{
 		width: 90%
@@ -105,9 +102,7 @@
 		background-color: #52CA3A;
 		color: black;
 	}
-	<form/>
 </style>
-</body>
 <body>
 	<div class="Web">
 		<div class="header">
@@ -141,7 +136,7 @@
 		<div class="banner">
 			<p>Toy ATN</p>
 			<div class="list">
-				<!-- <!-- <?php
+				 <?php
 				$connection = pg_connect("host=ec2-174-129-208-118.compute-1.amazonaws.com port=5432 dbname=d6hnv52hdi9nnk user=gpjcnwwhasdiya password=e9144a959f5174b3271698c559c6c557bce4d71494e9599fa793d33ede9b9436")
 				$sql = "SELECT * FROM Products";
 				$result = pg_query($connection, $sql);
@@ -155,7 +150,7 @@
 					</div>
 				<?php
 				}
-				?> --> -->
+				?> 
 			</div>
 		</div>
 	</div>
