62c98a6e1f59e61d325b739aa3a6f0d95e99cc73
Le Thanh Hung <hungltgch17366@fpt.edu.vn>
2019-05-04 09:27:17 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,33 +1,163 @@
+    
 <!DOCTYPE html>
 <html>
+<head>
+	<title>Demo</title>
+</head>
 <body>
+<style type="text/css">
+	<form>
+	.web
+	{
+		width: 90%
+		margin: 0 auto;
+	}
+	.button
+	{
+		float: right;
+		margin:11px;
+		padding-right: 40px;
+	}
+	.navbg
+	{
+		width: 100%;
+		height: 50px;
+		background-color:  #6EE1E5;
+	}
+	.buttonnav
+	{
+		float: right;
+		font-size: 200%;
+		padding-right: 5px;
+	}
+	.nav
+	{ 
+		margin-left: 350px;
+		width: 70%;
+		height: 50px;
+		background-color: #6EE1E5;
+		align-self: auto;
+	}
+	.header
+	{
+		width: 100%;
+		height: 208px;
+		background-color:white;
+	}
+	.header img{
+		max-width: 90%;
+		max-height: 99%;
+	}
+	.banner
+	{
+		width: 100%;
+		height: 1200px;
+	}
+	.banner p
+	{
+		font-size: 80px;
+		color: red;
+		text-align: center;
+		text-decoration: underline;
+	}
+	.list
+	{
+		width: 100%;
+		height: `1200px;
+		padding-top: 10px;
+		background-color: white;
+	}
+	.item
+	{
+		margin : 10px;
+		padding: 5px;
+		background-color: white;
+		width: 23%;
+		height: 320px;
+		float:left;
+		box-sizing: border-box;
+		text-align: center;
+	}
+	.nav ul
+	{
+		margin: 0 auto;
+		padding: 1px 30px;
+		list-style: none;
+		align-content: auto;
+	}
+	.nav a
+	{
+		padding: 0 10px;
+		text-decoration: none;
+		font-size: 30px;
+		color: black;
+		display: block;
+		line-height: 50px;
+	}
+	.nav li
+	{	margin: auto;
+		float: left;
+		padding: 0px 20px;
+			
+	}
+	.nav a:hover
+	{
+		background-color: #52CA3A;
+		color: black;
+	}
+	<form/>
+</style>
+</body>
+<body>
+	<div class="Web">
+		<div class="header">
+			<img src="https://i.ytimg.com/vi/Nn21sB-THvc/maxresdefault.jpg">
+			<div class="button">
+				  <div>
 
-<h1>My first PHP page</h1>
+					<form action="https://hungzz99.herokuapp.com/loign.php">
+						Hot line : 099-888-999
+						<input type="button" name="SignIn" value="Login">
+					</form>
+					
+				</div>
+			</div>			
+		</div>
+		<div class="navbg">
+			<form action="search.php" method="GET">
+			<div class="buttonnav"><input type="text" name="keyword" placeholder="Search...">
+				<input type="submit" name="Search" value="Search"></div>
+			</form>
+			<div class="nav">
+				<ul>
+					<li><a href="https://hungzz99.herokuapp.com/">Home</a></li>
+					<li><a >Category</a></li>
+					<li><a >Admin</a></li>
+					<li><a >FeedBack</a></li>
 
-<?php 
-$sql = "SELECT id, name FROM label";
-
-$db = parse_url(getenv("DATABASE_URL"));
-
-$pdo = new PDO("pgsql:" . sprintf(
-    "host=%s;port=%s;user=%s;password=%s;dbname=%s",
-    $db["host"],
-    $db["port"],
-    $db["user"],
-    $db["pass"],
-    ltrim($db["path"], "/")
-));
-
-$stmt = $pdo->prepare($sql);
-//Thiết lập kiểu dữ liệu trả về
-$stmt->setFetchMode(PDO::FETCH_ASSOC);
-$stmt->execute();
-$resultSet = $stmt->fetchAll();
-
-foreach ($resultSet as $row) {
-	echo $row['name'] . '\n';
-}
-
-?>
+				</ul><br>
+		</div>
+	</div>
+		<div class="banner">
+			<p>Toy ATN</p>
+			<div class="list">
+				<?php
+				$connection=pg_connect("host=ec2-174-129-208-118.compute-1.amazonaws.com port=5432 dbname=d6hnv52hdi9nnk user=gpjcnwwhasdiya password=e9144a959f5174b3271698c559c6c557bce4d71494e9599fa793d33ede9b9436")
+				$sql = "select * from Products";
+				$result = pg_query($connection, $sql);
+				while ($rows=pg_fetch_assoc($result))
+				{
+					?>
+					<div class="item">
+						<div class="tittle"><?=$rows['name']?></div>
+						<div class="image"><a href="index.php?proid=<?=$rows['proid']?>"><img src="<?=$rows['image']?>"></div></a>
+						<div class="tittle"><?=$rows['price']?></div>
+					</div>
+				<?php
+				}
+				?>
+			</div>
+		</div>
+	</div>
 </body>
 </html>
