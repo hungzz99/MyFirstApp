818e344df590874efd417b957d89105cc861db95
Le Thanh Hung <hungltgch17366@fpt.edu.vn>
2019-05-04 10:52:40 +0700

    Update demo2.php

--- demo2.php
+++ demo2.php
@@ -8,12 +8,11 @@
 $db = pg_connect("host=ec2-174-129-208-118.compute-1.amazonaws.com port=5432 dbname=d6hnv52hdi9nnk user=gpjcnwwhasdiya password=e9144a959f5174b3271698c559c6c557bce4d71494e9599fa793d33ede9b9436");
 $result = pg_query($db,"SELECT * FROM Products");
 ?>
-<div class="content">
-<table align="center" id="customers">
+<table align="center">
 	<tr><th align='center' width='200'>Name</th><th align='center' width='200'>Price ($)</th><th align='center' width='200'>Quantity</th><th align='center' width='200'>Image</th></tr>
 </table>
 <?php
-echo "<table align='center' id='customers'>";
+echo "<table align='center'>";
 while($row=pg_fetch_assoc($result)){echo "<tr>";
 echo "<td align='center' width='200'>" . $row['Column_2'] . "</td>";
 echo "<td align='center' width='200'>" . $row['Column_4'] . " $" . "</td>";
