1d459e7652b8a4378c3fe6df4f44e8ec73514d33
Le Thanh Hung <hungltgch17366@fpt.edu.vn>
2019-05-06 13:12:48 +0700

    Update admin.php

--- admin.php
+++ admin.php
@@ -134,8 +134,9 @@
 		<div class="header">
 			<img src="https://i.ytimg.com/vi/Nn21sB-THvc/maxresdefault.jpg">
 			<div class="button">
-				Hot line : 099-888-999
 				  <div>
+				  	
+				Hot line : 099-888-999
 <?php
 $db = pg_connect("host=ec2-174-129-208-118.compute-1.amazonaws.com port=5432 dbname=d6hnv52hdi9nnk user=gpjcnwwhasdiya password=e9144a959f5174b3271698c559c6c557bce4d71494e9599fa793d33ede9b9436");
 $result = pg_query($db,"SELECT * FROM login");
