86cb79b39197778344e430c059d38d595b0aff33
Le Thanh Hung <hungltgch17366@fpt.edu.vn>
2019-05-06 13:18:26 +0700

    Update admin.php

--- admin.php
+++ admin.php
@@ -135,18 +135,16 @@
 			<img src="https://i.ytimg.com/vi/Nn21sB-THvc/maxresdefault.jpg">
 			<div class="button">
 				  <div>
-				  	
-				Hot line : 099-888-999
 <?php
 $db = pg_connect("host=ec2-174-129-208-118.compute-1.amazonaws.com port=5432 dbname=d6hnv52hdi9nnk user=gpjcnwwhasdiya password=e9144a959f5174b3271698c559c6c557bce4d71494e9599fa793d33ede9b9436");
 $result = pg_query($db,"SELECT * FROM login");
 $rows=pg_fetch_assoc($result)
 ?>
-
 			<div class="hello"><?php echo "Hello, " . $rows['users'] ?></div>
 					
 				</div>
 				<form action="https://hungzz99.herokuapp.com/demo.php">
+					Hot Line: 099-888-999
 					<button> Log Out </button>
 				</form>
 			</div>			
